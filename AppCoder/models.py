ed7311d273ffc7b8cf3f59445ba3e80fa808cefc
Pedro Arrambide <p.arramabide@e-fmc.com.ar>
2023-08-23 21:02:47 -0300

    primer commit

---
+++ AppCoder/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+# Create your models here.
+
+class Curso(models.Model):
+    nombre = models.CharField(max_length=40)
+    camada =models.IntegerField()
+
+class Estudiante(models.Model):
+    mombre = models.CharField(max_length=30)
+    apelldio = models.CharField(max_length=30)
+    email = models.EmailField()
+
+class Profesor(models.Model):
+    mombre = models.CharField(max_length=30)
+    apelldio = models.CharField(max_length=30)
+    email = models.EmailField()
+    profesion = models.CharField(max_length=30)
+
+class Entregable(models.Model):
+    mombre = models.CharField(max_length=20)
+    fecha_entrega= models.DateField()
+    entregado = models.BooleanField()
+
+
+
